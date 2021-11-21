05a264e92bc399a4d77ab465c78fe2c7ddabdde2
MLDEV101011 <88951704+MLDEV101011@users.noreply.github.com>
2021-11-21 11:41:39 -0500

    init

---
+++ src/data.js
@@ -0,0 +1,64 @@
+import ashenSouls from './assets/ashenSouls.png'
+import blazorBank from './assets/blazorBank.PNG'
+import netflixClone from './assets/netflixClone.PNG'
+import theWatcher from './assets/theWatcher.png'
+export const projects = [
+  {
+    title: "Netflix Clone",
+    subtitle: "React, Firebase, API",
+    description:
+      "A clone of Netflix with login functionality using Google's Firebase database and data from Movies API.",
+    image: netflixClone,
+    link: "https://netflix-build-6b988.web.app/",
+  },
+  {
+    title: "Blazor Bank",
+    subtitle: "Bootstrap, HTML, CSS",
+    description:
+      "Front end for bank with pages for each department (Checking, Savings, Loans)",
+    image: blazorBank,
+    link: "https://themachetecoder.github.io/",
+  },
+  {
+    title: "Ashen Souls",
+    subtitle: "React Native",
+    description:
+      "Trivia game based on the Dark Souls video game franchise. Built using React Native.",
+    image: ashenSouls,
+    link: "https://github.com/MLDEV101011/souls",
+  },
+  {
+    title: "The Watcher",
+    subtitle: "C# ASP.Net MVC",
+    description:
+      "Database to store viewed movies and tv series with login functionality.",
+    image: theWatcher,
+    link: "https://github.com/MLDEV101011/WatcherDB",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "React Native",
+  "C#",
+  "Java",
+  "Python",
+];
